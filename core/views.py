0b6274c6251b31eed28a759648821f3814de2f71
Rafael Henrique <rafaelh9six@hotmail.com>
2022-07-01 21:22:13 -0300

    commit inicial

---
+++ core/views.py
@@ -0,0 +1,52 @@
+from email import message
+from django.shortcuts import render
+from django.contrib import messages
+
+from .forms import ContatoForm, ProdutoModelForm
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contato(request):
+    form = ContatoForm(request.POST or None)
+    
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            form.send_mail()
+            
+            
+            messages.success(request, 'E-mail enviado!')
+            form = ContatoForm()
+        else:
+            messages.error(request, "Erro ao enciar e-mail")   
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
+
+
+def produto(request):
+    if str(request.method) == 'POST':
+        form = ProdutoModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            prod = form.save(commit=False)
+            
+            print(f'Nome: {prod.nome}')
+            print(f'Preco: {prod.preco}')
+            print(f'Estoque: {prod.estoque}')
+            print(f'Imagem: {prod.imagem}')
+            
+            messages.success(request, 'Produto salvo com sucesso!')
+            form = ProdutoModelForm()
+        else:
+            messages.error(request, 'Erro ao salvar!')
+    else:
+        form = ProdutoModelForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'produto.html', context)
