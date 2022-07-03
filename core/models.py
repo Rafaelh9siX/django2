40be20e26efd81109ae4ab3817856beaf161cd32
Rafael Henrique <rafaelh9six@hotmail.com>
2022-07-03 14:27:07 -0300

    Exibindo produtos do banco no index.html, com modal exibindo a imagem do produto

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from stdimage.models import StdImageField
 
-# SINALS
+# SIGNALS
 from django.db.models import signals
 from django.template.defaultfilters import slugify
 
@@ -9,23 +9,25 @@ from django.template.defaultfilters import slugify
 class Base(models.Model):
     criado = models.DateField('Data de Criação', auto_now_add=True)
     modificado = models.DateField('Data de Atualização', auto_now=True)
-    ativo = models.BooleanField('Ativo', default=True)
-    
+    ativo = models.BooleanField('Ativo?', default=True)
+
     class Meta:
         abstract = True
-        
+
+
 class Produto(Base):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
     estoque = models.IntegerField('Estoque')
-    imagem = StdImageField('imagem', upload_to='produtos', variations={'thumb': (124, 124)})
+    imagem = StdImageField('Imagem', upload_to='produtos', variations={'thumb': (124, 124)})
     slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
-    
+
     def __str__(self):
         return self.nome
 
+
 def produto_pre_save(signal, instance, sender, **kwargs):
     instance.slug = slugify(instance.nome)
-    
-signals.pre_save.connect(produto_pre_save, sender=Produto)
 
+
+signals.pre_save.connect(produto_pre_save, sender=Produto)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
-from email import message
 from django.shortcuts import render
 from django.contrib import messages
+from django.shortcuts import redirect
 
 from .forms import ContatoForm, ProdutoModelForm
 from .models import Produto
@@ -15,17 +15,15 @@ def index(request):
 
 def contato(request):
     form = ContatoForm(request.POST or None)
-    
+
     if str(request.method) == 'POST':
         if form.is_valid():
             form.send_mail()
-            
-            
-            messages.success(request, 'E-mail enviado!')
+
+            messages.success(request, 'E-mail enviado com sucesso!')
             form = ContatoForm()
         else:
-            messages.error(request, "Erro ao enciar e-mail")   
-    
+            messages.error(request, 'Erro ao enviar e-mail')
     context = {
         'form': form
     }
@@ -33,20 +31,22 @@ def contato(request):
 
 
 def produto(request):
-    if str(request.method) == 'POST':
-        form = ProdutoModelForm(request.POST, request.FILES)
-        if form.is_valid():
-            
-            form.save()
-            
-            messages.success(request, 'Produto salvo com sucesso!')
-            form = ProdutoModelForm()
-        else:
-            messages.error(request, 'Erro ao salvar!')
-    else:
-        form = ProdutoModelForm()
-    context = {
-        'form': form
-    }
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
 
-    return render(request, 'produto.html', context)
+                form.save()
+
+                messages.success(request, 'Produto salvo com sucesso.')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar produto.')
+        else:
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context)
+    else:
+        return redirect('index')
